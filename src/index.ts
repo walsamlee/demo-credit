e6ddf342672d10cac0c52427ccf4188ac5e33129
Akinwale Samuel <wale0331@gmail.com>
2022-12-31 22:56:36 +0100

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
